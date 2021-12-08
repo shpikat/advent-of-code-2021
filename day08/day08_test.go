2c30bda7502c4b2ca011559b1aa08ff0da7861d2
Alexei Shpikat <a.shpikat@gmail.com>
2021-12-08 11:01:06 +0300

    Solve Day 08 :star:

---
+++ day08/day08_test.go
@@ -0,0 +1,74 @@
+package day08
+
+import (
+	"testing"
+
+	"github.com/shpikat/advent-of-code-2021/internal"
+)
+
+const (
+	sample1 = `
+be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
+`
+
+	part1Sample = 26
+	part1Answer = 440
+
+	part2Sample = 61229
+	part2Answer = 1046281
+)
+
+func TestPart1(t *testing.T) {
+	testCases := []struct {
+		name  string
+		input string
+		want  int
+	}{
+		{"sample 1", sample1, part1Sample},
+		{"puzzle input", internal.ReadInput(t, "./testdata/input.txt"), part1Answer},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := part1(tc.input)
+			if err != nil {
+				t.Errorf("error: %v", err)
+			}
+			if got != tc.want {
+				t.Errorf("got: %v, want: %v", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestPart2(t *testing.T) {
+	testCases := []struct {
+		name  string
+		input string
+		want  int
+	}{
+		{"sample 1", sample1, part2Sample},
+		{"puzzle input", internal.ReadInput(t, "./testdata/input.txt"), part2Answer},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := part2(tc.input)
+			if err != nil {
+				t.Errorf("error: %v", err)
+			}
+			if got != tc.want {
+				t.Errorf("got: %v, want: %v", got, tc.want)
+			}
+		})
+	}
+}
